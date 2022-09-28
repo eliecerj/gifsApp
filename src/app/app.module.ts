58dee787ba7278f0b90b3e5330a5b0433e56e725
Eliecer Arteaga <earteaga@sura.cl>
2022-09-27 23:10:17 -0300

    creacion de modulos y componentes sidebar, gifs, busqueda y resultados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private _historial: string[] = [];
+
+  get historial() {
+    return [... this._historial]; //romper la referencia ...
+  }
+
+  buscarGifs(query: string) {
+    this._historial.unshift(query);
+
+    console.log(this._historial);
+    
+  }
+
+}
