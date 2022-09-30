6283cfe2a59407d374eaf355e8ba385e36064208
Eliecer Arteaga <earteaga@sura.cl>
2022-09-29 23:21:35 -0300

    grilla resultados busqueda gifs, agrego tipado

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey: string = 'r5W7nUZc5vPz8NEakgUSFZIbp19pnPNK';
   private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
 
   get historial() {
     return [... this._historial]; //romper la referencia ...
   }
 
-  buscarGifs(query: string) {
-    this._historial.unshift(query);
+  constructor(private http: HttpClient ) {}
+  
+  buscarGifs(query: string = '') {
+
+    query = query.trim().toLowerCase();
+    
+    if( !this._historial.includes (query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+    }
+
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=r5W7nUZc5vPz8NEakgUSFZIbp19pnPNK&q=${query}&limit=10`)
+      .subscribe((resp) => {
+        console.log('resp', resp.data);
+        this.resultados = resp.data;
+        
+      })
 
     console.log(this._historial);
     
